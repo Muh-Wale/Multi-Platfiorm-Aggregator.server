63835fa3462cf2bb3cf28b0a3d10604ec3cfb691
Muh-Wale <muhammadolawale27@gmail.com>
2025-05-12 12:27:18 +0100

    connection issue

--- app.js
+++ app.js
@@ -13,18 +13,20 @@ app.use(cors({
     credentials: true,
 }));
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.get('/', (req, res) => {
     res.send('Hello Leave Here');
 });
+
 app.use('/api/users', userRoutes);
 app.use('/api/queries', queryRoutes);
 
-const server = app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// const server = app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
 
 module.exports = app;
